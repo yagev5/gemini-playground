481af5df0cdc1c2de64ff65eedcd6b3c736e2980
yagev5 <yagedeyouxiang@foxmail.com>
2025-08-16 21:50:51 +0800

    feat: Update default model to gemini-live-2.5-flash-preview

--- src/api_proxy/worker.mjs
+++ src/api_proxy/worker.mjs
@@ -141,7 +141,7 @@ async function handleEmbeddings (req, apiKey) {
   return new Response(body, fixCors(response));
 }
 
-const DEFAULT_MODEL = "gemini-1.5-pro-latest";
+const DEFAULT_MODEL = "gemini-live-2.5-flash-preview";
 async function handleCompletions (req, apiKey) {
   let model = DEFAULT_MODEL;
   switch(true) {
